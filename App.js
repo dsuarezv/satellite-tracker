5f4f3695f2982f5fce9cf9fb1b3f545bde7044b6
David Suárez <dsuarezv@gmail.com>
2019-09-01 19:37:12 +0100

    Improved three structure

--- App.js
+++ App.js
@@ -1,34 +1,107 @@
 import React, { Component } from 'react';
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 
 class App extends Component {
+
     componentDidMount() {
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        var cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-        camera.position.z = 5;
-        var animate = function () {
-            requestAnimationFrame(animate);
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            renderer.render(scene, camera);
-        };
-        animate();
+        this.sceneSetup();
+        this.addCustomSceneObjects();
+        this.startAnimationLoop();
+
+        window.addEventListener('resize', this.handleWindowResize);
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowResize);
+        window.cancelAnimationFrame(this.requestID);
+        this.controls.dispose();
+    }
+
+    sceneSetup = () => {
+        // get container dimensions and use them for scene sizing
+        const width = this.el.clientWidth;
+        const height = this.el.clientHeight;
+
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(
+            75, // fov = field of view
+            width / height, // aspect ratio
+            0.1, // near plane
+            1000 // far plane
+        );
+
+        this.camera = new THREE.PerspectiveCamera(
+            75, // fov = field of view
+            width / height, // aspect ratio
+            0.1, // near plane
+            1000 // far plane
+        );
+
+        this.controls = new OrbitControls(this.camera, this.el);
+
+        // after that this.controls might be used for enabling/disabling zoom:
+        // this.controls.enableZoom = false;
+
+        // set some distance from a cube that is located at z = 0
+        this.camera.position.z = 5;
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(width, height);
+        this.el.appendChild(this.renderer.domElement); // mount using React ref
+    };
+
+    addCustomSceneObjects = () => {
+        const geometry = new THREE.BoxGeometry(2, 2, 2);
+        const material = new THREE.MeshPhongMaterial({
+            color: 0x156289,
+            emissive: 0x072534,
+            side: THREE.DoubleSide,
+            flatShading: true
+        });
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
+
+        const lights = [];
+        lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+        lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+        lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+
+        lights[0].position.set(0, 200, 0);
+        lights[1].position.set(100, 200, 100);
+        lights[2].position.set(- 100, - 200, - 100);
+
+        this.scene.add(lights[0]);
+        this.scene.add(lights[1]);
+        this.scene.add(lights[2]);
+    };
+
+    startAnimationLoop = () => {
+        //this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+
+        this.renderer.render(this.scene, this.camera);
+        this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
+    };
+
+    handleWindowResize = () => {
+        const width = this.el.clientWidth;
+        const height = this.el.clientHeight;
+
+        this.renderer.setSize(width, height);
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+    };
 
     render() {
         const p = this.props;
 
         return (
-            <h1>Satellite tracker</h1>
+            <div>
+                <h1>Satellite tracker</h1>
+                <div ref={c => this.el = c} style={{ width: '640px', height: '400px' }} />
+            </div>
         )
     }
 }
