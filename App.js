80c4679d5b489a7bf651ed19834ecb482577f5a7
David Suárez <dsuarezv@gmail.com>
2019-09-01 16:46:08 +0100

    Initial import

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        const p = this.props;
+
+        return (
+            <h1>Satellite tracker</h1>
+        )
+    }
+}
+
+export default App;
