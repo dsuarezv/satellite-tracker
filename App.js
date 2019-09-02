2a7a6b039081a1d0e9cbad21ca37494ec7a13174
David Suárez <dsuarezv@gmail.com>
2019-09-02 12:53:11 +0100

    Refactor to support multiple satellites.

--- App.js
+++ App.js
@@ -7,12 +7,68 @@ import "./assets/theme.css";
 
 const EarthRadius = 6371;
 
+let TargetDate = new Date();
+
+const ISS = {
+    name: 'International Space Station ZARYA (NORAD 25544)',
+    category: 'B',
+    lte1: '1 25544U 98067A   19245.18443877  .00012516  00000-0  22337-3 0  9998',
+    lte2: '2 25544  51.6455 339.3385 0007918 357.2134  84.5192 15.50431138187200'
+}
+
+
+function parseLteFile (fileContent) {
+    const result = [];
+    const lines = fileContent.split("\n");
+    let current = null;
+
+    for (let i = 0; i < lines.length; ++i) {
+        const line = lines[i].trim();
+
+        if (line.length === 0) continue;
+
+        if (line[0] === '1') {
+            current.lte1 = line;
+        }
+        else if (line[0] === '2') {
+            current.lte2 = line;
+        }
+        else {
+            current = { name: line };
+            result.push(current);
+        }
+    }
+
+    return result;
+}
+
+
 class App extends Component {
+
+    state = {
+        selected: ISS
+    }
 
     componentDidMount() {
         this.setupScene();
-        this.setupLights();        
+        this.setupLights();
         this.addCustomSceneObjects();
+
+        this.addLteFileStations(`
+            ISS (ZARYA)
+            1 25544U 98067A   19245.37751584  .00005086  00000-0  95610-4 0  9995
+            2 25544  51.6456 338.3819 0007838 357.7936  82.3036 15.50423251187231
+            SWAS                    
+            1 25560U 98071A   19244.91605270  .00000082  00000-0  16284-4 0  9998
+            2 25560  69.8994  32.0710 0005902  37.6225 322.5343 14.93766324125789
+            GLOBALSTAR M023         
+            1 25621U 99004A   19244.91743841 -.00000120  00000-0 -90831-4 0  9995
+            2 25621  52.0131  13.5332 0010729 131.6589  20.9141 12.62379086948652
+            JCSAT-4A                
+            1 25630U 99006A   19244.93312921 -.00000226  00000-0  00000+0 0  9990
+            2 25630   3.9731  75.7694 0003126  68.2042 322.7520  1.00270705 75132`
+        );
+
         this.animationLoop();
 
         window.addEventListener('resize', this.handleWindowResize);
@@ -87,11 +143,24 @@ class App extends Component {
 
     addCustomSceneObjects = () => {
         this.addEarth();
-        this.addSatellite();
-        this.addOrbit();
     };    
 
-    
+    addLteFileStations = (fileContent) => {
+        const stations = parseLteFile(fileContent);
+
+        const material = new THREE.MeshPhongMaterial({
+            color: 0x00FF00,
+            emissive: 0xFF4040,
+            flatShading: false,
+            side: THREE.DoubleSide,
+        });
+
+        stations.forEach(s => {
+            this.addSatellite(s, material);
+        });
+    }
+
+
     // __ Scene contents ______________________________________________________
 
 
@@ -129,9 +198,9 @@ class App extends Component {
 
     }
 
-    addSatellite = () => {
+    addSatellite = (station, material) => {
         const geometry = new THREE.BoxBufferGeometry(50, 50, 50);
-        const material = new THREE.MeshPhongMaterial({
+        material = material || new THREE.MeshPhongMaterial({
             color: 0xFF0000,
             emissive: 0xFF4040,
             flatShading: false,
@@ -141,15 +210,15 @@ class App extends Component {
         this.sat = new THREE.Mesh(geometry, material);
         this.earth.add(this.sat);
 
-        this.updateSatPosition();
+        this.updateSatPosition(station);
+
+        //this.addOrbit(station);
     }
 
-    addOrbit = () => {
-        // For the next 24 hours, internvals of 30 minutes
-
+    addOrbit = (station) => {
         const intervalMinutes = 1;
-        const totalMinutes = 96;
-        const initialDate = new Date();
+        const totalMinutes = 102;
+        const initialDate = TargetDate;
 
         var material = new THREE.LineBasicMaterial({color: 0x999999});
         var geometry = new THREE.Geometry();
@@ -157,10 +226,7 @@ class App extends Component {
         for (var i = 0; i <= totalMinutes; i += intervalMinutes) {
             const date = new Date(initialDate.getTime() + i * 60000);
 
-            const pos = this.getPositionFromTLE(
-                '1 25544U 98067A   19245.18443877  .00012516  00000-0  22337-3 0  9998',   // ISS
-                '2 25544  51.6455 339.3385 0007918 357.2134  84.5192 15.50431138187200',
-                date);
+            const pos = this.getPositionFromTLE(station.lte1, station.lte2, date);
 
             geometry.vertices.push(new THREE.Vector3(pos.x, pos.y, pos.z));
         }        
@@ -169,11 +235,9 @@ class App extends Component {
         this.earth.add(orbitCurve);
     }
 
-    updateSatPosition = () => {
-        const pos = this.getPositionFromTLE(
-            '1 25544U 98067A   19245.18443877  .00012516  00000-0  22337-3 0  9998',    // ISS
-            '2 25544  51.6455 339.3385 0007918 357.2134  84.5192 15.50431138187200',
-        );
+    updateSatPosition = (station) => {
+        const pos = this.getPositionFromTLE(station.lte1, station.lte2);
+        if (!pos) return;
 
         this.sat.position.set(pos.x, pos.y, pos.z);
     }
@@ -194,9 +258,13 @@ class App extends Component {
     }
 
     getPositionFromTLE = (tle1, tle2, date) => {
+       
+        if (!tle1 || !tle2) {
+            return null;
+        }
 
         const satrec = satellite.twoline2satrec(tle1, tle2);
-        date = date || new Date();
+        date = date || TargetDate;
 
         const positionVelocity = satellite.propagate(satrec, date);
 
@@ -217,11 +285,14 @@ class App extends Component {
     
 
     render() {
+        const { selected } = this.state;
+
         return (
             <div>
                 <div className='Info'>
                     <h1>Satellite tracker</h1>
-                    <p>International Space Station (NORAD 25544)</p>
+                    <p>{TargetDate.toString()}</p>
+                    <p>{selected && selected.name}</p>
                     <p>Showing 1 day prediction</p>
                 </div>
                 <div ref={c => this.el = c} style={{ width: '100%', height: '100%' }} />
